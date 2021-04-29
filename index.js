be6917acfbfdffd3a43a0b42b2e9a51b5d88f979
Adam Okasha <okashaa@oregonstate.edu>
2021-04-28 20:45:23 -0400

    Update npm scripts, index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 
-const PORT = 4000;
+const PORT = process.env.NODE_ENV === "development" ? 4000 : process.argv[2];
 const app = express();
 
 app.engine(
@@ -134,7 +134,7 @@ app.get("/invoices/edit/:id", (req, res) => {
     // test data
     invoiceId: 1234,
     totalDue: 3000,
-    amountPaid: 1500
+    amountPaid: 1500,
   });
 });
 
