a442cb08255ce4df2054646eff4474b9dfa4e8f4
Adam Okasha <okashaa@oregonstate.edu>
2021-05-19 19:10:44 -0400

    Add invoices, bookings, customers service
    Co-authored-by: christofjs@gmail.com
    (cherry picked from commit 6fc77a57d17645000702c705c0380c8b0afab224)

---
+++ services/customers.service.js
@@ -0,0 +1,18 @@
+class CustomersService {
+  constructor({ customerModel }) {
+    this.customerModel = customerModel;
+  }
+  async findAll() {
+    return await this.customerModel.findAll();
+  }
+
+  async findOne(id) {
+    return await this.customerModel.findOne(id);
+  }
+
+  async create(customer) {
+    return await this.customerModel.create(customer);
+  }
+}
+
+module.exports = { CustomersService };
