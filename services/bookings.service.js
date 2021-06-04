e1629e2691bd498e42a241800450b18f091ef2f9
Adam Okasha <okashaa@oregonstate.edu>
2021-06-04 01:32:31 -0400

    Update booking service

--- services/bookings.service.js
+++ services/bookings.service.js
@@ -6,12 +6,27 @@ class BookingsService {
     return await this.bookingModel.findAll();
   }
 
-  async findOne({id, withCustomers = false}) {
-    return await this.bookingModel.findOne({id, withCustomers});
+  async findOne({ id, withCustomers = false }) {
+    return await this.bookingModel.findOne({ id, withCustomers });
   }
 
-  async create(booking) {
-    return await this.bookingModel.create(booking);
+  async create({ booking, customers }) {
+    const newBooking = await this.bookingModel.create(booking);
+    return await this.bookingModel.addCustomers({
+      customers,
+      booking_id: newBooking.insertId,
+    });
+  }
+
+  async addCustomers({ customer_ids, booking_id }) {
+    return await this.bookingModel.addCustomers({
+      customers: { customer_ids },
+      booking_id: booking_id,
+    });
+  }
+
+  async removeCustomer({ booking_id, customer_id }) {
+    return await this.bookingModel.removeCustomer({ booking_id, customer_id });
   }
 }
 
