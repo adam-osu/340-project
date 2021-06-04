dbc08baf09b8d8b0abffbc0539db9b0e302a1631
Adam Okasha <okashaa@oregonstate.edu>
2021-06-04 00:07:25 -0400

    update createBooking

--- public/javascript/createBooking.js
+++ public/javascript/createBooking.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("customer-search-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      new CustomerSearcher({
+        addButtonHandler: () => console.log("coolio"),
+      }).search();
+    });
+
   document.getElementById("add-booking").addEventListener("submit", (e) => {
     e.preventDefault();
 
