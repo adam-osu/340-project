556a8d82d1afcad529d09a38076d7b91aa5227d7
Adam Okasha <okashaa@oregonstate.edu>
2021-06-04 01:32:22 -0400

    Update booking model

--- models/booking.model.js
+++ models/booking.model.js
@@ -6,6 +6,8 @@ const {
   findAll,
   findOne,
   findBookingCustomers,
+  addCustomers,
+  removeCustomer,
 } = require("./queries/bookings.queries");
 class BookingModel extends BaseModel {
   constructor() {
@@ -24,7 +26,7 @@ class BookingModel extends BaseModel {
     });
   }
 
-  findOne({id, withCustomers}) {
+  findOne({ id, withCustomers }) {
     const bookingPromise = new Promise((resolve, reject) => {
       this.pool.query(findOne, [id], (err, rows) => {
         if (err) {
@@ -48,6 +50,39 @@ class BookingModel extends BaseModel {
     }
 
     return bookingPromise;
+  }
+
+  addCustomers({ customers, booking_id }) {
+    const data = customers.customer_ids.map((customer_id) => [
+      customer_id,
+      booking_id,
+      new Date(),
+    ]);
+
+    return new Promise((resolve, reject) => {
+      this.pool.query(addCustomers, [data], (err, rows) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(rows);
+      });
+    });
+  }
+
+  removeCustomer({ customer_id, booking_id }) {
+    console.log({customer_id, booking_id})
+    return new Promise((resolve, reject) => {
+      this.pool.query(
+        removeCustomer,
+        [customer_id, booking_id],
+        (err, rows) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(rows);
+        }
+      );
+    });
   }
 }
 
